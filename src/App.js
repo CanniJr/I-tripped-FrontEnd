0d0df285ff21b94b1df3b8aa978b1e7e2eb6be20
CanniJr <cannigialaluw@gmail.com>
2020-10-30 12:52:23 -0400

    added routers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Login from './Login'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+// users endpoint : http://localhost:4000/api/v1/users
+// trips endpoint : http://localhost:4000/api/v1/trips
+// places endpoint : http://localhost:4000/api/v1/places
+
+
+class App extends React.Component{
+
+  // componentDidMount(){
+  //   fetch('http://localhost:4000/api/v1/users')
+  //   .then(resp => resp.json())
+  //   .then(data => console.log(data))
+  // }
+
+  homePage = () => {
+    return (
+      <h1>this is the homepage!</h1>
+    )
+  }
+
+
+  render() {
+    return (
+      <Router>
+      <div>
+        <Route exact path ="/" component={this.homePage} />
+        <Route exact path="/login" component={Login} />
+      </div>
+      </Router>
+      );
+    }
+  }
+
+
+export default App
